9a8e977a2d090f6584272254eafaaa6d9cbd55c9
Ferdinand C. Espiritu II <168755183+itsferdss@users.noreply.github.com>
2025-02-20 16:08:28 +0800

    Add files via upload

---
+++ src/controllers/message.controller.js
@@ -0,0 +1,56 @@
+import User from "../model/message.model.js";
+import Message from "../model/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
+
+export const getUsersForSidebar = async (req, res ) => {
+    try {
+        const loggedInUserId = req.user._id;
+        const filteredUsers = await User.find({_id: {$ne: loggedInUserId}}). select("-password");
+        res.status(200).json(filteredUsers);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        res.status(500).json({ message: "Server Error" });
+    }
+}
+
+export const getMessages = async (req, res ) => {
+    try {
+        const{id:userToChatId} = req.params
+        const senderId = req.user._id;
+        const messages = await Message.find({
+            $or: [
+                { senderId: senderId, recieverId: userToChatId },
+                { senderId: userToChatId, recieverId: senderId },
+            ],
+        }).sort({ createdAt: 1 });
+        res.status(200).json(messages);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        res.status(500).json({ message: "Server Error" });
+    }
+}
+
+export const sendMessage = async (req, res ) => {
+    try {
+        const { id: recieverId } = req.params;
+        const { text, image } = req.body;
+        const senderId = req.user._id;
+        
+        let imageUrl;
+        if (image) {
+            const uploadResponse = await couldinary.uploader.upload(image);
+            imageUrl = uploadResponse.url;
+        }
+        const newMessage = new Message({
+            senderId,
+            recieverId,
+            text,
+            image: imageUrl,
+        });
+        await newMessage.save();
+        res.status(200).json(newMessage);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        res.status(500).json({ message: "Server Error" });
+    }
+}
